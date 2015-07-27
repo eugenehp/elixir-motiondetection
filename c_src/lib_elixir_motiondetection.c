bc7f3b93a828e22a88234077f61c961dfdb73ff0
Eugene HP <eugene.hp2012@gmail.com>
2015-07-27 07:48:48 +0200

    initial NIF test

---
+++ c_src/lib_elixir_motiondetection.c
@@ -0,0 +1,23 @@
+#include "erl_nif.h"
+
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define MAXBUFLEN 1024
+
+static ERL_NIF_TERM _test(ErlNifEnv* env, int arc, const ERL_NIF_TERM argv[])
+{
+  char path[MAXBUFLEN];
+
+  enif_get_string(env, argv[0], path, 1024, ERL_NIF_LATIN1);
+  
+  return enif_make_string(env, "Hello World!", ERL_NIF_LATIN1);
+}
+
+static ErlNifFunc nif_funcs[] =
+{
+  {"_test", 1, _test}
+};
+
+ERL_NIF_INIT(Elixir.Motiondetection,nif_funcs,NULL,NULL,NULL,NULL)
--- lib/motiondetection.ex
+++ lib/motiondetection.ex
@@ -1,2 +1,16 @@
 defmodule Motiondetection do
+	 @on_load :init
+
+	def init() do
+		:erlang.load_nif("./priv_dir/lib_elixir_motiondetection", 0)
+		:ok
+	end
+
+	def test(inputString) do
+		_test(String.to_char_list(inputString))
+	end
+
+	def _test(inputString) do
+		"NIF library not loaded #{inputString}"
+	end
 end
